1f46d26f26b6c36897c77e51e70fed87c005fcce
KhaNhi <KhaNhi@users.noreply.github.com>
2024-04-20 15:44:20 +0700

    timphantulonnhat

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap vao so luong hang cua mang:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap vao so luong cot cua mang:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        int[,] myarry = new int[x,y]; 
+        Console.WriteLine("Nhap tung gia tri vao mang !");
+        for (int row = 0; row < x;row++)
+        {
+            for ( int col = 0; col < y;col++)
+            {
+                Console.WriteLine("Nhap vao gia tri phan tu ({0},{1})",row,col);
+                myarry[row,col] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        int max = 0;
+        Console.WriteLine("Mang da tao la:");
+        for (int row = 0; row < x;row++)
+        {
+            
+            for ( int col = 0; col < y;col++)
+            {
+                if (myarry[row,col] > max)
+                {
+                    max = myarry[row,col];
+                }
+                Console.Write(myarry[row,col]+" ");
+            }
+            Console.WriteLine("\n");
+        }
+        Console.WriteLine("Phan tu co gia tri lon nhat la: {0}",max);
+    }
+}
